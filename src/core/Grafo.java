c506da84a69cc1340e00fa8a1c147cd211d9ce88
Cristhian Fontana Mattiuzzi <20191BSI0077@cefetes.br>
2022-04-18 09:02:04 -0300

    grafoGeradoPorFuncao

--- src/core/Grafo.java
+++ src/core/Grafo.java
@@ -5,8 +5,7 @@
  */
 package core;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.IOException;
 /**
  *
  * @author 20191bsi0077
@@ -15,8 +14,9 @@ public class Grafo {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Grafo1 g = new Grafo1();
         ManipulationFile file = new ManipulationFile();
         file.generateFork(100);
@@ -53,5 +53,27 @@ public class Grafo {
 //        }
         
         
+    }
+    
+    public static void grafoGeradoPorFuncao () throws IOException {
+        Grafo1 g = new Grafo1();
+        ManipulationFile file = new ManipulationFile();
+        file.generateFork(100);
+        
+        g.load("data.txt");
+        g.mostraGrafo1(g.wg, "Grafo de Entrada");
+        
+        long tempInitDijkstra = System.currentTimeMillis();
+        g.dijkstra(g.wg, "V1");
+        long tempDijkstra = System.currentTimeMillis() - tempInitDijkstra;
+
+        System.out.println("o metodo Dijkstra executou em " + Long.toString(System.currentTimeMillis() - tempInitDijkstra));
+        
+        long tempInitBellmanford = System.currentTimeMillis();
+        g.menorCaminhoorigemUnica(g.wg, "V1"); // bellman_ford
+        long tempBellmanford = System.currentTimeMillis() - tempInitBellmanford;
+        
+        System.out.printf("O metodo Bellman Ford executou em: %d milisegundos\n", tempDijkstra);
+        System.out.printf("O metodo Dijkstra executou em: %d milisegundos\n", tempBellmanford);
     }
 }
