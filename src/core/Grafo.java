909b7cd71b145e8a19a98fbd828ce7a4bd6ccafe
Cristhian <cristhian762@gmail.com>
2022-04-27 18:33:46 -0300

    main grafo

--- src/core/Grafo.java
+++ src/core/Grafo.java
@@ -21,8 +21,9 @@ public class Grafo {
 	public static void main(String[] args) throws IOException {
 		Grafo1 g = new Grafo1();
 		ManipulationFile file = new ManipulationFile();
-		file.generateFork(1000);
-//        
+
+		file.generateFork(100);
+
 		g.load("data.txt");
 		g.mostraGrafo1(g.wg, "Grafo de Entrada");
 
@@ -36,32 +37,5 @@ public class Grafo {
 
 		System.out.printf("O metodo Bellman Ford executou em: %d milisegundos\n", tempDijkstra);
 		System.out.printf("O metodo Dijkstra executou em: %d milisegundos\n", tempBellmanford);
-
-//		g.load("g7.txt");
-//		g.mostraGrafo1(g.wg, "Grafo de Entrada");
-//		long tempInitDijkstra = System.currentTimeMillis();
-//		g.dijkstra(g.wg, "A");
-//		long tempDijkstra = System.currentTimeMillis() - tempInitDijkstra;
-//
-//		long tempInitBellmanford = System.currentTimeMillis();
-//		g.menorCaminhoorigemUnica(g.wg, "A"); // bellman_ford
-//		long tempBellmanford = System.currentTimeMillis() - tempInitBellmanford;
-//
-//		System.out.printf("O metodo Bellman Ford executou em: %d milisegundos\n", tempDijkstra);
-//		System.out.printf("O metodo Dijkstra executou em: %d milisegundos\n", tempBellmanford);
-//        G.add("g1.txt");
-//        G.add("g2.txt");
-//        G.add("g3.txt");
-//        G.add("g4.txt");
-//        G.add("g5.txt");
-//        G.add("g6.txt");
-//          g.add("g7.txt");
-//
-//        for (String grafo : G) {
-//            g.load(grafo);
-//            g.mostraGrafo1(g.wg, "Grafo de Entrada");
-//            g.dijkstra(g.wg, "A");
-//            g.menorCaminhoorigemUnica(g.wg, "A"); // bellman_ford
-//        }
 	}
 }
